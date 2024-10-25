f281b56de5da0ca8e6f7bbd2b1df96ba657bb243
fadelanumah <fadelanumah@gmail.com>
2024-10-25 20:56:37 +0700

    fix(home): Fix Font Size

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,9 +4,11 @@ import { Button } from './ui/button'
 const Hero = () => {
   return (
     <div className='flex flex-col gap-6 h-screen items-center justify-center'>
-      <h1 className='font-bold text-7xl'>Toserba</h1>
-      <h2 className='font-regular text-3xl'>Toko Serba Bayar 🛒</h2>
-      <Button>Scroll untuk melihat daftar barang yang bisa dibayar</Button>
+      <h1 className='font-bold text-3xl md:text-5xl'>Toserba</h1>
+      <h2 className='font-regular text-xl md:text-2xl'>Toko Serba Bayar 🛒</h2>
+      <div>
+        <Button size="sm">Scroll untuk melihat daftar barang yang bisa dibayar</Button>
+      </div>
     </div>
   )
 }
